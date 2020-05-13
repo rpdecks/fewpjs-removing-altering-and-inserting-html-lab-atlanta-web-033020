f4f87acff768e031820a76746e0f8d4811554f27
rpdecks <rpdecks@gmail.com>
2020-05-12 21:48:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const element = document.querySelector('#main');
+element.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.body.appendChild('newHeader');
